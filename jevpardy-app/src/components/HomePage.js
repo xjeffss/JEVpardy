8541f3f603382e5420ca99ddd853e73d5fea0d10
re41969 <charboneauedwarda@johndeere.com>
2021-02-22 12:54:20 -0600

    found picture

--- jevpardy-app/src/components/HomePage.js
+++ jevpardy-app/src/components/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../HomePage.css'
+
 
 function HomePage(){
     return(
         <div>
-            Home Page
+         <h1>Welcome to JEVpardy App</h1>
+
+         <img src= "https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2015-02/640_snl_40_jeopardy.jpg?itok=Zu7LWFGh" />
+    
             
         </div>
     )
