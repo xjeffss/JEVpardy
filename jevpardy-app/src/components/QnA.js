35f16942a4d2d3d183577a550757efb84ccc0207
xjeffss <elmorejeffreyp@johndeere.com>
2021-02-22 14:38:14 -0600

    QnA, API good CSS TV styling

--- jevpardy-app/src/components/QnA.js
+++ jevpardy-app/src/components/QnA.js
@@ -3,17 +3,28 @@ import '../QnA.css';
 
 function QnA(props){
     console.log(props.question[0])
+
     return(
+
         <div>
           {props.apiDataLoaded && 
-          <div>
+          <div className="tv">
           <div className="question"> 
               {props.question[0].question}
-              </div>
-            <div className="answer">
-              {props.question[0].answer}
+            
+            <div className="answer" >
+           
+                <br/>
+                <br/>
+              Answer:
+                <br/>
+              {props.question[0].answer}</div>
             </div>
           </div>}
+          <div className="answerButtons">
+              <div className="right">I got it RIGHT!</div>
+              <div className="wrong">I got it wrong!</div>
+          </div>
         </div>
     )
 }
