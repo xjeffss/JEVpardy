e0074303331c77b74ad35a81acd7c50e16fb83f9
vincentm <matthiasvincentm@johndeere.com>
2021-02-22 09:32:42 -0600

    created components files and set up App.js and have API data loading

--- jevpardy-app/src/App.js
+++ jevpardy-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      questions: [],
+      apiDataLoaded: false,
+    }
+  }
+
+  componentDidMount = async() => {
+    const jevArray= await axios.get('http://jservice.io/api/clues')
+    console.log(jevArray.data)
+     this.setState({
+        questions: jevArray.data,
+        apiDataLoaded: true,
+     })
+  }
+
+  render() {
+    return (
+      <div className="App">   
+        <header>
+          <h1>
+            JEVpardy
+          </h1>
+        </header>  
+
+          
+      </div>
+    );
+  }
 }
 
 export default App;
