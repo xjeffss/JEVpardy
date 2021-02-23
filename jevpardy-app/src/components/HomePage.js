10e0739e919771a3251b2585e6e7d6ff44db9806
re41969 <charboneauedwarda@johndeere.com>
2021-02-23 13:16:18 -0600

    merge

--- jevpardy-app/src/components/HomePage.js
+++ jevpardy-app/src/components/HomePage.js
@@ -5,15 +5,17 @@ import '../HomePage.css'
 function HomePage(){
     return(
         <div>
-        
+        <div className="image-text-wrapper">
         <body>  
 
 
         <h1 className="Title" >Welcome to JEVpardy App</h1>
+
         <button>Start Game</button>
 
 
         </body>  
+        </div>
         </div>
     )
 }
